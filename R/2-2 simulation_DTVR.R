6547fb6e0486f33958701c6abb45ec8db1abd929
hunyong.cho <hunyong.cho@gmail.com>
2015-09-07 22:16:59 -0500

    miscellaneous

--- R/2-2 simulation_DTVR.R
+++ R/2-2 simulation_DTVR.R
@@ -2,7 +2,6 @@ library(MASS)
 
 
 set.seed(10)
-# set.seed(10)
 
 # should segregate D=0 and D=1 for R
 datagenerator <- function(n, alpha0, alpha1, beta0, beta1, beta2, beta3, sig, q1, q2, q3, q4, gamma ,mu.V,Sigma, DT.only=FALSE){
@@ -29,13 +28,14 @@ datagenerator <- function(n, alpha0, alpha1, beta0, beta1, beta2, beta3, sig, q1
     return(data.frame(disease=D, marker=T))
   }
 }
+parm.set <- list()
 
 mu.V = rep(0,5)
 Sigma = matrix(c(1, 0, 0.3, 0.4, -0.4, 0, 1, 0.2, 0.2, 0, 0.3, 0.2, 1, 0.7, -0.5, 0.4, 0.2, .7, 1, -0.2, -0.4, 0, -0.5, -0.2, 1), 5,5)
 phi = 0.5; alpha0 = log(phi/(1-phi))
 alpha1 = rep(1,5)
 beta0 = 0
-beta1 = 0.3484
+### beta1 = 0.9 below->
 beta2 = rep(0.1,5)
 beta3 = rep(0.05,5)
 sig = 1
@@ -58,16 +58,21 @@ gamma = 0.2
   mean(a)
 }
 
-a <- Sys.time(); temp <- datagenerator(n=100000, alpha0=alpha0, alpha1=alpha1, beta0=beta0, beta1=beta1, beta2=beta2, beta3=beta3, sig=sig, q1=q1, q2=q2, q3=q3, q4=q4, gamma=gamma, mu.V=mu.V, Sigma=Sigma, missing=FALSE) ; Sys.time()-a
+beta1 = 1.9767
+a <- Sys.time(); temp <- datagenerator(n=100000, alpha0=alpha0, alpha1=alpha1, beta0=beta0, beta1=beta1, beta2=beta2, beta3=beta3, sig=sig, q1=q1, q2=q2, q3=q3, q4=q4, gamma=gamma, mu.V=mu.V, Sigma=Sigma, DT.only=TRUE) ; Sys.time()-a
 a <- Sys.time(); AUC(temp$marker[temp$disease==0], temp$marker[temp$disease==1]); Sys.time()-a
 
 # theta finder
-a <- Sys.time(); n=50000000; n1=1000000; n2 = floor(n/n1)
-temp <- data.frame(disease=rep(NA,n), marker=NA)
-for (i in 1:n2) {temp[((i-1)*n1+1):(i*n1),] <- datagenerator(n=n1, alpha0=alpha0, alpha1=alpha1, beta0=beta0, beta1=beta1, beta2=beta2, beta3=beta3, sig=sig, q1=q1, q2=q2, q3=q3, q4=q4, gamma=gamma, mu.V=mu.V, Sigma=Sigma, DT.only=TRUE)} ; Sys.time()-a
-a <- Sys.time(); AUC(temp$marker[temp$disease==0], temp$marker[temp$disease==1]); Sys.time()-a
-
-
+a <- Sys.time(); n=10000000; n1=1000000; n2 = floor(n/n1)
+set.seed(400)
+for (k in 1:5) {
+  tht = rep(NA,10); for (j in 1:10) {
+    temp <- data.frame(disease=rep(NA,n), marker=NA)
+    for (i in 1:n2) {temp[((i-1)*n1+1):(i*n1),] <- datagenerator(n=n1, alpha0=alpha0, alpha1=alpha1, beta0=beta0, beta1=beta1, beta2=beta2, beta3=beta3, sig=sig, q1=q1, q2=q2, q3=q3, q4=q4, gamma=gamma, mu.V=mu.V, Sigma=Sigma, DT.only=TRUE)} ; Sys.time()-a
+    a <- Sys.time(); tht[j] <- AUC(temp$marker[temp$disease==0], temp$marker[temp$disease==1]); Sys.time()-a
+  }
+  print(tht)
+}
 
 head(temp)
 Sys.time(); AUC(temp$marker[temp$disease==0], temp$marker[temp$disease==1])
