9e56fec8a651949febecd71aac1de9ca07ad155b
hunyong.cho <hunyong.cho@gmail.com>
2015-09-25 00:55:43 -0500

    datagenerator options

--- R/2-2 simulation_DTVR.R
+++ R/2-2 simulation_DTVR.R
@@ -1,38 +1,40 @@
-library(MASS)
+library(MASS)  # for mvrnorm
 
 
 set.seed(10)
 
 # should segregate D=0 and D=1 for R
-datagenerator <- function(n, alpha0, alpha1, beta0, beta1, beta2, beta3, sig, q1, q2, q3, q4, gamma ,mu.V,Sigma, DT.only=FALSE){
+datagenerator <- function(n, alpha0, alpha1, beta0, beta1, beta2, beta3, sig, q1, q2, q3, q4, gamma ,mu.V,Sigma, option="VDTR"){
   V = mvrnorm(n, mu.V, Sigma)
   phi = (1+ exp(alpha0 + V%*%alpha1)^-1)^-1
   D = rbinom(n, 1, phi)
-  mu.T = beta0 + D*beta1 + V%*%beta2 + D*V%*%beta3
-  T = mu.T + sig*rnorm(n)
-  if (DT.only == FALSE) {
-    # for R(missingness)
-    R.T = (T <= quantile(T, q1))*(1-D) + (T <= quantile(T, q3))*D
-    Q2 = apply(V, 2, quantile, q2)
-    Q4 = apply(V, 2, quantile, q4)
-    normalV = t(apply(V,1,"<=",Q2))*(1-D) + t(apply(V,1,"<=",Q4))*D
-    R.V = as.vector(t(apply(normalV,1,prod)))
-    rho = (R.T*R.V)*gamma
-    R = rbinom(n, 1, rho)
-    DR = ifelse(R==0, D, NA)    
-    return(data.frame(disease=D, marker=T, V=V, R=R, diseaseR=DR))
-  }
-  else {
-    R = 1
-    DR = D
-    return(data.frame(disease=D, marker=T))
+  if (option == "VD") {
+    return(data.frame(disease=D, V=V))
+  } else {
+    mu.T = beta0 + D*beta1 + V%*%beta2 + D*V%*%beta3
+    T = mu.T + sig*rnorm(n)
+
+    if (option == "VDR") {
+      return(data.frame(disease=D, marker=T))
+    } else {  # option="VDTR"
+      # for R(missingness)
+      R.T = (T <= quantile(T, q1))*(1-D) + (T <= quantile(T, q3))*D
+      Q2 = apply(V, 2, quantile, q2)
+      Q4 = apply(V, 2, quantile, q4)
+      normalV = t(apply(V,1,"<=",Q2))*(1-D) + t(apply(V,1,"<=",Q4))*D
+      R.V = as.vector(t(apply(normalV,1,prod)))
+      rho = (R.T*R.V)*gamma
+      R = rbinom(n, 1, rho)
+      DR = ifelse(R==0, D, NA)    
+      return(data.frame(disease=D, marker=T, V=V, R=R, diseaseR=DR))
+    }
   }
 }
 parm.set <- list()
 
 mu.V = rep(0,5)
 Sigma = matrix(c(1, 0, 0.3, 0.4, -0.4, 0, 1, 0.2, 0.2, 0, 0.3, 0.2, 1, 0.7, -0.5, 0.4, 0.2, .7, 1, -0.2, -0.4, 0, -0.5, -0.2, 1), 5,5)
-phi = 0.5; alpha0 = log(phi/(1-phi))
+alpha0 = 1.6111   # for phi=0.7
 alpha1 = rep(1,5)
 beta0 = 0
 ### beta1 = 0.9 below->
@@ -47,7 +49,7 @@ gamma = 0.2
 
 
 ### theta_(1+2+..+10) almost equals average(theta_1, ... theta_10) 
-{ temp <- datagenerator(10000,alpha0,alpha1,beta0,beta1,beta2,beta3,sig,gamma0,gamma1,gamma2,gamma3,mu.V,Sigma)  
+{ temp <- datagenerator(10000, alpha0=alpha0, alpha1=alpha1, beta0=beta0, beta1=beta1, beta2=beta2, beta3=beta3, sig=sig, q1=q1, q2=q2, q3=q3, q4=q4, gamma=gamma, mu.V=mu.V, Sigma=Sigma, option="VDT")  
   AUC(temp$marker[temp$disease==0], temp$marker[temp$disease==1])
   a <- c(1:10)
   for (i in 1:10){
@@ -58,26 +60,16 @@ gamma = 0.2
   mean(a)
 }
 
-beta1 = 1.9767
-a <- Sys.time(); temp <- datagenerator(n=100000, alpha0=alpha0, alpha1=alpha1, beta0=beta0, beta1=beta1, beta2=beta2, beta3=beta3, sig=sig, q1=q1, q2=q2, q3=q3, q4=q4, gamma=gamma, mu.V=mu.V, Sigma=Sigma, DT.only=TRUE) ; Sys.time()-a
-a <- Sys.time(); AUC(temp$marker[temp$disease==0], temp$marker[temp$disease==1]); Sys.time()-a
+### free parameters: alpha0(for phi), beta1(for theta)
+### To estimate parameters that satisfy specific phi and theta,
+### refer to 2-2-1 parameterfinder.R(beta1) and 2-2-2 alphafinder.R(alpha0).
+alphabet = data.frame(phi = rep(c(.5,.7), each=3), theta = rep(c(.7,.8,.9),2), alpha0 = rep(c(0,1.6111), each=3), beta1=rep(NA,6))
+alphabet$beta1 = c(0.8089, 1.4486, 1.97674, NA, NA, NA)
 
-# theta finder
-a <- Sys.time(); n=10000000; n1=1000000; n2 = floor(n/n1)
-set.seed(400)
-for (k in 1:5) {
-  tht = rep(NA,10); for (j in 1:10) {
-    temp <- data.frame(disease=rep(NA,n), marker=NA)
-    for (i in 1:n2) {temp[((i-1)*n1+1):(i*n1),] <- datagenerator(n=n1, alpha0=alpha0, alpha1=alpha1, beta0=beta0, beta1=beta1, beta2=beta2, beta3=beta3, sig=sig, q1=q1, q2=q2, q3=q3, q4=q4, gamma=gamma, mu.V=mu.V, Sigma=Sigma, DT.only=TRUE)} ; Sys.time()-a
-    a <- Sys.time(); tht[j] <- AUC(temp$marker[temp$disease==0], temp$marker[temp$disease==1]); Sys.time()-a
-  }
-  print(tht)
-}
 
 head(temp)
 Sys.time(); AUC(temp$marker[temp$disease==0], temp$marker[temp$disease==1])
-Sys.time(); performance(prediction(temp$marker, temp$disease),"auc")@y.values[[1]]
-Sys.time()
+
 #temp[,c("disease","diseaseR")]
 table(data.frame(disease=temp[,c("disease")], diseaseR=ifelse(is.na(temp$diseaseR),"NA",temp$disease)))
 
