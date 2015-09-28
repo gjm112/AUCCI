cf3439f2e4f819ee61fdc8045909ea1c151dc132
hunyong.cho <hunyong.cho@gmail.com>
2015-09-27 23:21:02 -0500

    New: alpha finder

---
+++ R/2-3-Apdx2 [SIM] AlphaFinder.R
@@ -0,0 +1,19 @@
+### Part II. Simulation
+## 2-3 Apdx2 [SIM] Simulator - Appendix. parameter finder(alpha0 for phi)
+
+phi = 0.7; alpha0 = log(phi/(1-phi))
+alpha0 =1.61107
+n.sample = 10000
+pb <- txtProgressBar(min=0, max = n.sample, char = paste0("="), style=3)
+
+a <- Sys.time(); phi.hat = rep(NA,n.sample)
+set.seed(300)
+for (i in 1:n.sample){
+  setTxtProgressBar(pb,i)
+  temp <- datagenerator(n=1000000, alpha0=alpha0, alpha1=alpha1, beta0=beta0, beta1=beta1, beta2=beta2, beta3=beta3, sig=sig, q1=q1, q2=q2, q3=q3, q4=q4, gamma=gamma, mu.V=mu.V, Sigma=Sigma, option="VD") ; Sys.time()-a
+  phi.hat[i] = mean(temp$disease)
+  if (i %% floor(n.sample/30) == 0) {print(mean(phi.hat, na.rm=TRUE))}
+}
+mean(phi.hat)-phi;mean(phi.hat) ; sd(phi.hat); sd(phi.hat)/sqrt(n.sample)
+Sys.time() - a
+## phi.hat.16110 = phi.hat
