687d4de3ba701d5015da8c972967a9163c0f0d81
hunyong.cho <hunyong.cho@gmail.com>
2015-10-10 21:47:15 -0500

    made applicable for logit transformation and multiple imputation

--- R/1-2 [CI] AUCCI (Complete).R
+++ R/1-2 [CI] AUCCI (Complete).R
@@ -2,7 +2,7 @@
 ## 1-2 [CI] AUCCI(Complete): 1.2.1 AUCCI, 1.2.2 CI.i
 
 ## 1.2.1 AUC CI #######################################################################
-AUCCI <- function(data, method, disease="disease", marker="marker", alpha=0.05, variance = FALSE, cc = FALSE) {
+AUCCI <- function(data, method, disease="disease", marker="marker", alpha=0.05, LT=FALSE, variance = FALSE, cc = FALSE) {
   ### note for developers
   ## method
   # "HanleyMcNeilWald", "HanleyMcNeilExponential", "HanleyMcNeilScore", ...
@@ -10,59 +10,110 @@ AUCCI <- function(data, method, disease="disease", marker="marker", alpha=0.05, 
   # to show estimated variance also or not?
   ## cc: logical
   # continuity correction (for Wilson Score only)
+  ## LT: logit transformation (HMW,HME, NE, HMS, NS, CM, Bb, MW, DL)  ( RG, HalpM, DB, DG, CP, WS)
+  # For Score-like methods(WS, HalpM) or parametric methods(RG,DB,DG), LT is not necessary.
+  # returning variance when logit=T is var(logit(theta))
   
   # basic stats
   x = data[data[,disease]==0, marker]
   y = data[data[,disease]==1, marker]
   n.x = length(x) ; n.y = length(y) ; n = n.x + n.y
   A = AUC(x, y, n.x, n.y)
+  A.LT = logit(A, LT=LT)
   
   if (method == "HanleyMcNeilWald") {
     Q = Q.stat(x, y, n.x, n.y); Q1 = Q$Q1; Q2 = Q$Q2
-    V = V.HM(A, n.x, n.y, Q1, Q2)
-    CI = CI.base(A,V,alpha)
+    V.LT = V.HM(A, n.x, n.y, Q1, Q2,LT=LT)
+    CI.LT = CI.base(A.LT,V.LT,alpha)
   } 
   
   else if (method == "HanleyMcNeilExponential") {
-    V = V.HM(A, n.x, n.y)
-    CI = CI.base(A,V,alpha)  
+    V.LT = V.HM(A, n.x, n.y, LT=LT)
+    CI.LT = CI.base(A.LT,V.LT,alpha)
   }
   
   else if (method == "HanleyMcNeilScore") {
-    CI = multiroot(HMS.equation, c(0.01,0.99), AUC.hat=A, n.x=n.x, n.y=n.y, alpha=alpha)$root
-    if (variance) {V = V.HM(A, n.x, n.y)}
+    CI = multiroot(HMS.equation, c(0.5,0.9), AUC.hat=A, n.x=n.x, n.y=n.y, alpha=alpha, LT=LT)$root  #original scale
+    CI.LT = logit(CI, LT=LT)
+    if (variance) {V.LT = V.HM(A, n.x, n.y, LT=LT)}
   }
   
   else if (method == "NewcombeExponential") {
-    V = V.NC(A, n.x, n.y)
-    if (V < 0) { warning("Negative estimated variance set to zero")
-                 V = 0 }
-    CI = CI.base(A,V,alpha)  
+    V.LT = V.NC(A, n.x, n.y, LT=LT)
+    if (V.LT < 0) { warning("Negative estimated variance set to zero")
+                 V.LT = 0 }
+    CI.LT = CI.base(A.LT,V.LT,alpha)
   }
   
   else if (method == "NewcombeScore") {
-    CI = multiroot(NC.equation, c(0.01,0.99), AUC.hat=A, n.x=n.x, n.y=n.y, alpha=alpha)$root
+    CI = multiroot(NC.equation, c(0.5,0.9), AUC.hat=A, n.x=n.x, n.y=n.y, alpha=alpha, LT=LT)$root
+    CI.LT = logit(CI, LT=LT)
     if (variance) {
-      V = V.NC(A, n.x, n.y)
-      if (V < 0) { warning("Negative estimated variance set to zero")
-                   V = 0 }      
+      V.LT = V.NC(A, n.x, n.y, LT=LT)
+      if (V.LT < 0) { warning("Negative estimated variance set to zero")
+                   V.LT = 0 }      
     }
   }
   
   else if (method == "CortesMohri") {
-    V = V.CM(A, n.x, n.y)
-    CI = CI.base(A,V,alpha)
+    V.LT = V.CM(A, n.x, n.y, LT=LT)
+    CI.LT = CI.base(A.LT,V.LT,alpha)
   }
   
   else if (method == "ReiserGuttman") {
-    mu.x = mean(x); mu.y = mean(y)
-    sig.x = sd(x);  sig.y = sd(y); s2 = sig.x^2 + sig.y^2
-    d = (mu.y-mu.x)/sqrt(s2)
-    f = s2^2/((sig.x^4/(n.x-1))+(sig.y^4/(n.y-1)))
-    M = s2/((sig.x^2/n.x)+(sig.y^2/n.y))
-    V = 1/M + d^2/(2*f)
+    probit = probit.RG(x,y,n.x,n.y)
+    d = probit$delta; V = probit$V
+    A = pnorm(d)
     d.CI = d + c(-1,1)*qnorm(1-alpha/2)*sqrt(V)
     CI = pnorm(d.CI)
+    CI.LT = logit(CI, LT=LT) # logit transformation not available for RG (only for presentation purpose)
+    V.LT = NA                # variance not available for RG(The V above is variance in probit scale)
+  }
+  
+  else if (method == "Bamber") {
+    V.LT = V.Bm(x, y, n.x, n.y, A, LT=LT)
+    CI.LT = CI.base(A.LT,V.LT,alpha)
+  }
+
+  else if (method == "HalperinMee") {
+    CI = multiroot(Halperin.equation, c(0.5,0.9), AUC.hat=A, x=x, y=y, n.x=n.x, n.y=n.y, alpha=alpha, LT=LT)$root
+    CI.LT = logit(CI, LT=LT)
+    if (variance) {
+      V.LT=V.Halperin(x, y, n.x=n.x, n.y=n.y, LT=LT) 
+    }
+  }
+  
+  else if (method == "DoubleBeta") {
+    CI = DB(A, n.x, n.y, alpha, LT=LT)
+    CI.LT = logit(CI, LT=LT)
+    if (variance) {
+      alp = multiroot(DB.equation, c(1,3), AUC.hat=A, n.x=n.x, n.y=n.y, alpha=1,LT=LT)$root[1]
+      V.LT = V.DB(alp, n.x, n.y, LT=LT)
+    }
+  }
+  
+  else if (method == "DoubleGaussian") {
+    CI = DG(A, n.x, n.y, alpha, LT=LT)
+    CI.LT = logit(CI, LT=LT)
+    if (variance) {
+      delta = multiroot(DG.equation, c(1,3), AUC.hat=A, n.x=n.x, n.y=n.y, alpha=1,LT=LT)$root[1]
+      V.LT = V.DG(delta, n.x, n.y, LT=LT)
+    }
+  }
+  
+  else if (method == "MannWhitney") {
+    V = S.stat(x, y, n.x, n.y)^2 * n / (n.x * n.y);  V.LT = V/ifelse(LT,(A*(1-A))^2,1)
+    CI = CI.base(A.LT,V.LT,alpha)
+  }
+  
+  else if (method == "DeLong") {
+    V = V.DeLong(x, y, n.x, n.y, A);  V.LT = V/ifelse(LT,(A*(1-A))^2,1)
+    CI = CI.base(A.LT,V.LT,alpha)
+  }  
+  
+  else if (method == "WilsonScore") {
+    z.a2 = qnorm(1-alpha/2)
+    CI = (2*n*A + z.a2^2 +cc*c(-1,1) +c(-1,1)* z.a2* sqrt(z.a2^2 + cc*c(-2,2) - 1/n*cc + 4*A*(n*(1-A) +1*cc)))/(2*(n+z.a2^2))
   }
   
   # V for CP???
@@ -74,75 +125,30 @@ AUCCI <- function(data, method, disease="disease", marker="marker", alpha=0.05, 
     CI = c( (k*f1)/(n-k+1+k*f1), ((k+1)*f2)/(n-k+(k+1)*f2) )
   }
   
-  else if (method == "Bamber") {
-    V = V.Bm(x, y, n.x, n.y, A)
-    CI = CI.base(A,V,alpha)
-  }
-  
-  else if (method == "WilsonScore") {
-    z.a2 = qnorm(1-alpha/2)
-    CI = (2*n*A + z.a2^2 +cc*c(-1,1) +c(-1,1)* z.a2* sqrt(z.a2^2 + cc*c(-2,2) - 1/n*cc + 4*A*(n*(1-A) +1*cc)))/(2*(n+z.a2^2))
-  }
-  
-  else if (method == "HalperinMee") {
-    CI = Halperin(x, y, n.x, n.y, alpha)
-    if (variance) {
-      Halperin.stat = Halperin.stat(x, y, n.x, n.y)
-      AUC.hat = Halperin.stat$AUC.hat; N.J.hat = Halperin.stat$N.J.hat
-      V = AUC.hat*(1-AUC.hat)/N.J.hat
-    }
-  }
-  
-  # V for DB: TBD
-  else if (method == "DoubleBeta") {
-    CI = DB(A, n.x, n.y, alpha)
-  }
-  
-  # V for DG: TBD
-  else if (method == "DoubleGaussian") {
-    CI = DG(A, n.x, n.y, alpha)
-  }
-  
-  else if (method == "MannWhitney") {
-    V = S.stat(x, y, n.x, n.y)^2 * n / (n.x * n.y)
-    CI = CI.base(A,V,alpha)
-  }
-  
-  else if (method == "MannWhitneyLT") {
-    V = S.stat(x, y, n.x, n.y)^2 * n / (n.x * n.y)
-    CI = CI.base(log(A/(1-A)), V/ (A*(1-A))^2, alpha)
-    CI = exp(CI) / (exp(CI) + 1)
-  }
-  
-  else if (method == "DeLong") {
-    V = V.DeLong(x, y, n.x, n.y, A)
-    CI = CI.base(A,V,alpha)
-  }  
-  
-  result = list(AUC.hat = A, CI = CI);  if (variance) {result$V.hat = V}
+  result = list(AUC.hat = A, CI = expit(CI.LT,LT=LT));  if (variance) {result$V.hat = V.LT}
   return(result)
 }
 
 
 ## 1.2.2 lumpsum AUCCI for simulation purpose #########################################
 # CI's for individual elements of a sample (1. landscape form)  2 (lb,ub) x 17
-CI.i <- function(data, method=CI.methods, type="landscape2", ...) {
+CI.i <- function(data, fun=AUCCI, CI.method=CI.methods, type="landscape2", ...) {
   if (type == "landscape1") {
-    CI.i = as.data.frame(matrix(NA,2,length(method)+1))
-    names(CI.i) = c("AUC.hat", method)
-    CI.i$AUC.hat = AUCCI(data, method[1])$AUC.hat
-    for (i in 1:length(method)) { CI.i[,i+1] = AUCCI(data, method = method[i], ...)$CI }
+    CI.i = as.data.frame(matrix(NA,2,length(CI.method)+1))
+    names(CI.i) = c("AUC.hat", CI.method)
+    CI.i$AUC.hat = fun(data, CI.method[1],...)$AUC.hat
+    for (i in 1:length(CI.method)) { CI.i[,i+1] = fun(data, CI.method = CI.method[i], ...)$CI }
   } else if (type == "landscape2") {
-    CI.i = as.data.frame(matrix(NA,1,length(method)*2+1))
+    CI.i = as.data.frame(matrix(NA,1,length(CI.method)*2+1))
     names(CI.i) = c("AUC.hat",paste0(rep(CI.methods,each=2),c(".lb",".ub")))
-    CI.i$AUC.hat = AUCCI(data, method[1])$AUC.hat
-    for (i in 1:length(method)) { CI.i[1,(2*i):(2*i+1)] = AUCCI(data, method = method[i], ...)$CI }  
+    CI.i$AUC.hat = fun(data, CI.method[1],...)$AUC.hat
+    for (i in 1:length(CI.method)) { CI.i[1,(2*i):(2*i+1)] = fun(data, CI.method = CI.method[i], ...)$CI }  
   } else if (type=="portrait") {
-    CI.i = as.data.frame(matrix(NA,length(method)+1,2))
+    CI.i = as.data.frame(matrix(NA,length(CI.method)+1,2))
     names(CI.i) = c("lowerbound", "upperbound")
-    rownames(CI.i)  = c("AUC.hat", method)
-    CI.i[1,] = AUCCI(data, method[1])$AUC.hat
-    for (i in 1:length(method)) { CI.i[i+1,] = AUCCI(data, method = method[i], ...)$CI }  
+    rownames(CI.i)  = c("AUC.hat", CI.method)
+    CI.i[1,] = fun(data, CI.method[1],...)$AUC.hat
+    for (i in 1:length(CI.method)) { CI.i[i+1,] = fun(data, CI.method = CI.method[i], ...)$CI }  
   } else {stop("Form is none of landscape1, landscape2, or portrait")}    
   return(CI.i)
 }
