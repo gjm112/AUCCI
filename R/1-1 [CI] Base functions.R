ca7293d7a0aacb508b6a5e27371e05153dc955ce
hunyong.cho <hunyong.cho@gmail.com>
2015-10-13 16:22:37 -0500

    drop some methods: Wilson, CP, MannWhitney Function added: Rubin's variance estimator

--- R/1-1 [CI] Base functions.R
+++ R/1-1 [CI] Base functions.R
@@ -5,18 +5,19 @@
 library(rootSolve)  # for Newton-Raphson Method (0.2.4)
 library(sn)         # for Owen's T-function in Double Gaussian Method (0.2.15)
 
-# CI methods :    ref to 1.CI test(temp), 2.simulation, 3.evaluation
+# CI methods :    ref to 2-3.[SIM] Simulator, 3.evaluation
 CI.methods = c("HanleyMcNeilWald", "HanleyMcNeilExponential", "HanleyMcNeilScore", 
                "NewcombeExponential", "NewcombeScore", "CortesMohri", "ReiserGuttman", 
-               "Bamber", "HalperinMee", "DoubleBeta", "DoubleGaussian",
-               "MannWhitney", "DeLong", 
-               "ClopperPearson","WilsonScore")
+               "Bamber", "HalperinMee", "DoubleBeta", "DoubleGaussian", "DeLong")
+
+###               "ClopperPearson","WilsonScore" => excluded from this study: not AUC specific
+###               "MannWhitney"                   => excluded: seems identical to DeLong
 ########################################################################################
 
 ## 1.1.1 Basic internal functions ######################################################
 # CI.base, AUC.classic, AUC
-CI.base <- function(mu.hat, Var, alpha) {
-  return(mu.hat + c(-1,1) * qnorm(1-alpha/2)*sqrt(Var))
+CI.base <- function(mu.hat, Var, alpha, dist=qnorm,...) {
+  return(mu.hat + c(-1,1) * dist(1-alpha/2,...)*sqrt(Var))
 }
 AUC.classic <- function(x, y, n.x=length(x), n.y=length(y)) {
   A <- 0; for (i in 1:n.x) { A = A + ( sum(y > x[i]) + sum(y==x[i])/2 ) }
@@ -56,6 +57,27 @@ expit <- function(data, LT=TRUE) {
   if (LT) {return((1+exp(data)^-1)^-1)} else {return(data)}
 }
 
+# variance estimator for multiple imputation
+Rubin = function(W, MI, alpha=0.05, print.r=FALSE, print.nu=FALSE) {
+  if (is.na(MI[1])) {
+    v.final = W
+    z.val=qnorm(1-alpha/2)
+    r = 0
+    nu = NA
+  } else {
+    m = length(MI)
+    B = var(MI)
+    v.final = W + B*(1+1/m)
+    r = v.final / W  - 1   # missing ratio
+    nu = (m-1)*(1+1/r)
+    z.val = qt(1-alpha/2, nu)
+  }
+  result = data.frame(v.final=v.final, z.val=z.val)
+  if (print.r) {result$r = r}
+  if (print.nu) {result$nu = nu}
+  return(result)
+}
+
 ## 1.1.2 variance estimators ###########################################################
 # Q.stat for HM and its derivatives
 Q.stat <- function(x, y, n.x=length(x), n.y=length(y)) {
@@ -69,18 +91,35 @@ V.HM <- function(AUC, n.x, n.y, Q1 = AUC/(2-AUC), Q2=2* AUC^2 /(AUC + 1), LT=FAL
   if (LT) {div.LT = (AUC*(1-AUC))^2} else {div.LT=1}
   return((AUC * (1-AUC) + (n.y -1)*(Q1 - AUC^2) + (n.x-1)*(Q2 - AUC^2)) /(n.x*n.y) /div.LT )
 }
+###1 ################## Formula 1: fixed B.hat
 HMS.equation = function(AUC, AUC.hat, n.x, n.y, alpha, MI=NA, LT=FALSE) {
-  if (is.na(MI[1])) {B=0; m=1} else {m = length(MI); B = var(MI)}
-  logit(AUC,LT=LT) + c(+1,-1)*qnorm(1-alpha/2) * sqrt(V.HM(AUC, n.x, n.y, LT=LT)+B*(1+1/m)) - logit(AUC.hat,LT=LT)
+  W = V.HM(AUC, n.x, n.y, LT=LT)
+  Rubin = Rubin(W=W, MI=MI, alpha=alpha)
+  V = Rubin$v.final         # W + (1/m)*B for MI
+  z.val = Rubin$z.val       # t value for MI
+  return(logit(AUC,LT=LT) + c(+1,-1)*z.val*sqrt(V) - logit(AUC.hat,LT=LT))
 }
+###2 ################## Formula 2: fixed r.hat
+HMS.equation = function(AUC, AUC.hat, n.x, n.y, alpha, MI=NA, LT=FALSE) {
+  W = V.HM(AUC, n.x, n.y, LT=LT)
+  W.hat = V.HM(AUC.hat, n.x, n.y, LT=LT)
+  Rubin = Rubin(W=W.hat, MI=MI, alpha=alpha, print.r=TRUE)
+  r = Rubin$r
+  z.val = Rubin$z.val       # nu is fixed (since r is fixed)
+  return(logit(AUC,LT=LT) + c(+1,-1)*z.val*sqrt(W*(1+r)) - logit(AUC.hat,LT=LT))
+}
+
 V.NC <- function(AUC, n.x, n.y, LT=FALSE) {
   N = (n.x+n.y)/2
   if (LT) {div.LT = (AUC*(1-AUC))^2} else {div.LT=1}
   return( AUC*(1-AUC)/(n.x*n.y) * ( (2*N-1) - (3*N-3) / ((2-AUC)*(AUC+1)) )/div.LT )
 }
 NC.equation <- function(AUC, AUC.hat, n.x, n.y, alpha, MI=NA, LT=FALSE) {
-  if (is.na(MI[1])) {B=0; m=1} else {m = length(MI); B = var(MI)} 
-  logit(AUC,LT=LT) + c(+1,-1)*qnorm(1-alpha/2) * sqrt(V.NC(AUC, n.x, n.y, LT=LT)+B*(1+1/m)) - logit(AUC.hat,LT=LT)
+  W = V.NC(AUC, n.x, n.y, LT=LT)
+  Rubin = Rubin(W=W, MI=MI, alpha=alpha)
+  V = Rubin$v.final         # W + (1/m)*B for MI
+  z.val = Rubin$z.val       # t value for MI
+  return(logit(AUC,LT=LT) + c(+1,-1)*z.val*sqrt(V) - logit(AUC.hat,LT=LT))
 }
 V.CM <- function(AUC, n.x, n.y, LT=FALSE){
   z1 = function (w, n.x, n.y, k) {1-0.5*(w/n.x+(k-w)/n.y)}
@@ -152,8 +191,11 @@ V.Halperin <- function(x, y, n.x=length(x), n.y=length(y), AUC=AUC(x,y,n.x,n.y),
   return( V/div.LT )
 }
 Halperin.equation <- function(AUC, AUC.hat, x, y, n.x, n.y, alpha, MI=NA, LT=FALSE){
-  if (is.na(MI[1])) {B=0; m=1} else {m = length(MI); B = var(MI)}
-  logit(AUC,LT=LT) + c(+1,-1)*qnorm(1-alpha/2) * sqrt(V.Halperin(x, y, n.x, n.y, AUC, LT=LT)+B*(1+1/m)) - logit(AUC.hat,LT=LT)
+  W = V.Halperin(x, y, n.x, n.y, AUC, LT=LT)
+  Rubin = Rubin(W=W, MI=MI, alpha=alpha)
+  V = Rubin$v.final         # W + (1/m)*B for MI
+  z.val = Rubin$z.val       # t value for MI
+  return(logit(AUC,LT=LT) + c(+1,-1)*z.val*sqrt(V) - logit(AUC.hat,LT=LT))
 }
 V.DB <- function(alp, n.x, n.y, LT=FALSE) {
   R1 = gamma(alp + 1)^2 / gamma(2*alp + 1)
@@ -161,12 +203,15 @@ V.DB <- function(alp, n.x, n.y, LT=FALSE) {
   theta = 1 - R1
   Q = 1 - 2*R1 + R2
   if (LT) {div.LT = (theta*(1-theta))^2} else {div.LT=1}
-  V = (theta*(1-theta) + (n.x+n.y-2)*(Q-theta^2))/n.x/n.y/div.LT
+  return(V = (theta*(1-theta) + (n.x+n.y-2)*(Q-theta^2))/n.x/n.y/div.LT)
 }
 DB.equation <- function(alp, AUC.hat, n.x, n.y, alpha, MI=NA, LT=FALSE){
-  if (is.na(MI[1])) {B=0; m=1} else {m = length(MI); B = var(MI)}
   AUC = 1 - gamma(alp + 1)^2 / gamma(2*alp + 1)
-  return(logit(AUC,LT=LT) + c(+1,-1)*qnorm(1-alpha/2) * sqrt(V.DB(alp, n.x, n.y, LT=LT)+B*(1+1/m)) - logit(AUC.hat,LT=LT))
+  W = V.DB(alp, n.x, n.y, LT=LT)
+  Rubin = Rubin(W=W, MI=MI, alpha=alpha)
+  V = Rubin$v.final         # W + (1/m)*B for MI
+  z.val = Rubin$z.val       # t value for MI
+  return(logit(AUC,LT=LT) + c(+1,-1)*z.val*sqrt(V) - logit(AUC.hat,LT=LT))
 }
 DB <- function(AUC.hat, n.x, n.y, alpha,...) {
   alp <- multiroot(DB.equation, c(1,3), AUC.hat=AUC.hat, n.x=n.x, n.y=n.y, alpha=alpha,...)$root
@@ -180,25 +225,30 @@ V.DG <- function(delta, n.x, n.y, LT=FALSE){
   return(V = ((n.x + n.y - 1)*theta*(1-theta) - 2*(n.x + n.y -2) * Owen ) / (n.x * n.y) /div.LT)
 }
 DG.equation <- function(delta, AUC.hat, n.x, n.y, alpha, MI=NA, LT=FALSE) {
-  if (is.na(MI[1])) {B=0; m=1} else {m = length(MI); B = var(MI)}
+  W = V.DG(delta, n.x, n.y, LT=LT)
+  Rubin = Rubin(W=W, MI=MI, alpha=alpha)
+  V = Rubin$v.final         # W + (1/m)*B for MI
+  z.val = Rubin$z.val       # t value for MI
   AUC = pnorm(delta/sqrt(2))
-  return(logit(AUC,LT=LT) + c(+1,-1)*qnorm(1-alpha/2) * sqrt(V.DG(delta, n.x, n.y, LT=LT)+B*(1+1/m)) - logit(AUC.hat,LT=LT))
+  return(logit(AUC,LT=LT) + c(+1,-1)*z.val*sqrt(V) - logit(AUC.hat,LT=LT))
 }
 DG <- function(AUC.hat, n.x, n.y, alpha,...) {
   delta <- multiroot(DG.equation, c(0.5, 0.9), AUC.hat=AUC.hat, n.x=n.x, n.y=n.y, alpha=alpha,...)$root
   return(pnorm(delta/sqrt(2)))
 }
-S.stat <- function(x, y, n.x=length(x), n.y=length(y)) {
+S.stat <- function(x, y, n.x=length(x), n.y=length(y), A = AUC(x, y, n.x, n.y), LT=FALSE) {
   ranks = rank(c(x,y))
   rank.x = ranks[1:n.x] ; rank.y = ranks[-1:-n.x]   # ranks within both x and y
   rank.i = rank(rank.x) ; rank.j = rank(rank.y)     # ranks within each
   S2.10 = ( sum((rank.x - rank.i)^2) - n.x*(mean(rank.x) - (n.x+1)/2)^2 ) / ((n.x-1)*n.y^2)
   S2.01 = ( sum((rank.y - rank.j)^2) - n.y*(mean(rank.y) - (n.y+1)/2)^2 ) / ((n.y-1)*n.x^2)
-  return(S = sqrt((n.x * S2.01 + n.y * S2.10)/(n.x + n.y)))
+  if (LT) {div.LT = (A*(1-A))^2} else {div.LT=1}
+  return(S = sqrt((n.x * S2.01 + n.y * S2.10)/(n.x + n.y)/div.LT))
 }
-V.DeLong <- function(x, y, n.x=length(x), n.y=length(y), A = AUC(x, y, n.x, n.y)) {
+V.DeLong <- function(x, y, n.x=length(x), n.y=length(y), A = AUC(x, y, n.x, n.y), LT=FALSE) {
   D2 <- D1 <- 0
   for (i in 1:n.x) {D1 = D1 + ( sum(y > x[i]) / n.y - A )^2}; D1 = D1 /(n.x * (n.x - 1))
   for (j in 1:n.y) {D2 = D2 + ( sum(y[j] > x) / n.x - A )^2}; D2 = D2 /(n.y * (n.y - 1))
-  return(V = D1 + D2)
+  if (LT) {div.LT = (A*(1-A))^2} else {div.LT=1}
+  return(V = (D1 + D2)/div.LT)
 }
